f5ca11542f1dd6703a276369effccec0b550be40
unknown <shakthibala499@gmail.com>
2022-06-19 01:06:06 +0530

    layout over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParticipantsComponentComponent } from './main/videoCallPage/participants-component/participants-component.component';
+import { VideoCallComponent } from './main/videoCallPage/video-call/video-call.component';
+import { VideoComponentComponent } from './main/videoCallPage/video-component/video-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoCallComponent,
+    ParticipantsComponentComponent,
+    VideoComponentComponent
   ],
   imports: [
     BrowserModule,
