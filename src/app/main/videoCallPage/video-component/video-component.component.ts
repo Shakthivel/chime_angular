07ae631336a75f471767173f88fbef3167b907bd
unknown <shakthibala499@gmail.com>
2022-06-19 14:17:11 +0530

    grid layout over

--- src/app/main/videoCallPage/video-component/video-component.component.ts
+++ src/app/main/videoCallPage/video-component/video-component.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponentComponent implements OnInit {
 
+
+  speakerOff:boolean = false; 
+  micOff:boolean = true;
+  camOff:boolean = true;
+  shareOff:boolean = true;
+
+  isScreenPinned:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeSpeakerStatus(){this.speakerOff = ! this.speakerOff;}
+  changeMicStatus(){this.micOff = ! this.micOff;}
+  changeCamStatus(){this.camOff = !this.camOff;}
+  changeScreenShareStatus(){this.shareOff = !this.shareOff;}
+  changePinStatus(){
+    this.isScreenPinned = !this.isScreenPinned;
+  }
 }
