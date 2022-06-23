4f318469e718568f6ae85d2715bcc40b3fbb9ccd
JaswanthReddyPadala <jaswanthreddypadala@gmail.com>
2022-06-23 18:16:19 +0530

    Added Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomePageContentComponent } from './main/homePage/home-page-content/home-page-content.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateMeetingComponent } from './main/createMeetingPage/create-meeting/create-meeting.component';
@@ -6,7 +7,7 @@ import { TestpageComponent } from './main/testpage/testpage.component';
 import { VideoCallComponent } from './main/videoCallPage/video-call/video-call.component';
 
 const routes: Routes = [
-  { path: '', component: JoinMeetingComponent },
+  { path: '', component: HomePageContentComponent },
   { path: 'join', component: JoinMeetingComponent },
   { path: 'create', component: CreateMeetingComponent },
   { path: 'test', component: TestpageComponent },
